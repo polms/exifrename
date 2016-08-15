fdb7c9ce39003ca7e7158d634fbc2452baec1e64
Robin MOALIC <robin.moalic@yahoo.fr>
2016-08-15 15:29:36 +0200

    memoire

--- main.c
+++ main.c
@@ -8,13 +8,14 @@
 void timeffile(struct tm *tm, char *file_name) {
 	ExifData *exif;
 	ExifEntry *entry;
-	char date[20];
 
 	exif = exif_data_new_from_file(file_name);
 	entry = exif_content_get_entry(exif->ifd[EXIF_IFD_EXIF], EXIF_TAG_DATE_TIME_ORIGINAL);
-	exif_entry_get_value(entry, date, 20);
 
-	strptime(date, "%Y:%m:%d %H:%M:%S", tm);
+	strptime((const char *)entry->data, "%Y:%m:%d %H:%M:%S", tm);
+	
+//	exif_entry_unref(entry); // libere la mémoire (unref appele free)
+	exif_data_unref(exif);
 }
 
 void disptime(struct tm *tm) {
